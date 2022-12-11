68c9be2f53c24ec48d137779fbfc4f0124079742
ValerieKhumalo <mkhumalo293@gmail.com>
2022-12-10 23:06:06 -0800

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-	printf(1, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	writef(1, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
