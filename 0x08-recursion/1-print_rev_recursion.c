fc3fe8c010b35afe455498962c6b767cc454fca7
ValerieKhumalo <mkhumalo293@gmail.com>
2023-01-05 16:31:15 -0800

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string, follwed by a new line
+ * @s: string
+ * Return: no return.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
